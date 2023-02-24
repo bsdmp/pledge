c437dc280c8f288f5a413c603fc2dbc8046bd0fd
Mikhail <mp39590@gmail.com>
2023-02-24 19:10:25 +0300

    WIP

--- pledge.c
+++ pledge.c
@@ -3,13 +3,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <err.h>
-#include <limits.h>
 
 void
-usage(int i)
+usage(void)
 {
-	fprintf(stderr, "%s %i: [-p path mode ...] <-P \"promises\"> "
-	    "<program> [arguments]\n", getprogname(), i);
+	fprintf(stderr, "%s: [-p path mode ...] <-P \"promises\"> "
+	    "<program> [arguments]\n", getprogname());
 	exit(1);
 }
 
@@ -24,20 +23,15 @@ main(int argc, char **argv)
 	for (i = 1; i < argc; i++) {
 		if (strcmp(argv[i], "-P") == 0) {
 			if (Pflag)
-				usage(0);
+				usage();
 			Pflag = 1;
 			strlcpy(promises, argv[++i], sizeof(promises));
 			break;
 		}
 		if (strcmp(argv[i], "-p") == 0) {
 			if (pflag)
-				usage(0);
+				usage();
 			pflag = 1;
-			if (Pflag) {
-				fprintf(stderr, "%s: -P flag must be before "
-				    "-p\n", getprogname());
-				exit(1);
-			}
 			i++;
 			j = 0;
 			while (argv[i] != NULL && strcmp(argv[i], "-P") != 0) {
@@ -45,37 +39,30 @@ main(int argc, char **argv)
 				    sizeof(*paths));
 				modes = reallocarray(modes, j+1,
 				    sizeof(*modes));
-				//printf("argv[%i]=%s\n", i, argv[i]);
 				paths[j] = strdup(argv[i++]);
-				//printf("paths[%i]=%s\n", j, paths[j]);
-				//printf("argv[%i]=%s\n", i, argv[i]);
 				/* We've advanced 'i', check if it's not EOL */
 				if (argv[i] == NULL)
-					usage(1);
+					usage();
 				modes[j++] = strdup(argv[i++]);
-				//printf("modes[%i]=%s\n", j, modes[j-1]);
-				//printf("===\n");
 			}
 			/* If we have no args after -p */
 			if (j == 0)
-				usage(2);
+				usage();
 			/* Last cycle advanced i to -P, shift it back */
 			i--;
 			continue;
 		}
 	}
 
+	/* -P is mandatory */
 	if (Pflag == 0)
-		usage(3);
+		usage();
 
-	//printf("---\n");
 	if (pledge("stdio exec unveil", promises) == -1)
 		err(1, "pledge");
 
 	if (pflag) {
 		for (k = 0; k < j; k++) {
-			//printf("paths[%i]=%s\n", k, paths[k]);
-			//printf("modes[%i]=%s\n", k, modes[k]);
 			if (unveil(paths[k], modes[k]) == -1)
 				err(1, "unveil: %s mode %s", paths[k],
 				    modes[k]);
